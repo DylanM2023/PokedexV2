81f267098e750fb797cd1b656b6942ed41e3903d
DylanM2023 <145995742+DylanM2023@users.noreply.github.com>
2025-02-11 09:47:27 +0000

    Created react project

---
+++ frontend/src/pages/pokemon_view.jsx
@@ -0,0 +1,15 @@
+import { useEffect, useState } from "react";
+
+const PokeView = () => {
+    
+    const [pokedata, setPokeData] = useState([])
+
+    useEffect(()=>{
+        
+    }, [])
+    //Logic
+    
+    return ( <><h1>This is the poke view page!!</h1></> );
+}
+ 
+export default PokeView;
