4bb07ffc37ec5677a8c48355f3d56d581bf4122a
DylanM2023 <145995742+DylanM2023@users.noreply.github.com>
2025-02-17 05:16:30 +0000

    Added search functionality

--- frontend/src/pages/pokemon_view.jsx
+++ frontend/src/pages/pokemon_view.jsx
@@ -3,24 +3,24 @@ import { Pokedex } from "pokeapi-js-wrapper";
 
 const PokeView = () => {
 
+    const [name, setName] = useState('')
     const pokedex = new Pokedex()
     const [pokemon, setPokemon] = useState('');
 
-    useEffect(()=>{
-        async function fetchPoke () {
+    async function fetchPoke (name) {
             try{
-                const pokemon_data = await pokedex.getPokemonByName("pikachu")
+                const pokemon_data = await pokedex.getPokemonByName(name)
                 setPokemon(pokemon_data)
                 }
                 catch(e){console.error(e)}
         };
-        fetchPoke()
-    }, [])
 
     console.log(pokemon)
 
     return ( 
     <>
+        <input type="text" placeholder="Search For Pokemon" onChange={(e) => {setName(e.target.value)}}/>
+        <button onClick={()=>{fetchPoke(name)}}>Search</button>
         {pokemon != '' && <div><h1>{pokemon.name}</h1>
         <img className="" src={pokemon.sprites.other['official-artwork'].front_default} alt="Image Unavailable" />
         <p>{pokemon.abilities.effect_entries}</p>
