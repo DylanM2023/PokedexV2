7b3e9835301668fbd5567d33809bbfe8fe19a13a
DylanM2023 <145995742+DylanM2023@users.noreply.github.com>
2025-02-24 22:11:18 +0000

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,6 +5,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    allowedHosts: "pokedexv2.onrender.com"
+    allowedHosts: ["pokedexv2.onrender.com"]
   }
 })
