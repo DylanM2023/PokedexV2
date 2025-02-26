2e448989295bef7bdf339e53a7ac413b4a96e8de
DylanM2023 <145995742+DylanM2023@users.noreply.github.com>
2025-02-26 11:46:13 +0000

    Changed source for app

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/pokedexV2"
+  base: "https://github.com/DylanM2023/PokedexV2"
 })
