11c23c94cf669aa2d19dbfa16989a11c1eaa60fd
DylanM2023 <145995742+DylanM2023@users.noreply.github.com>
2025-02-26 12:56:50 +0000

    Tried fixing on render error

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
   server: {
     allowedHosts : ["pokedexv2.onrender.com"],
-  }
+  },
+  optimizeDeps:{
+    include: ['react/jsx-runtime']
+  },
+
+  plugins: [react()],
 })
